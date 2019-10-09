40c481a4df88ce18d29610960547196443597e32
Fede <fedemc@gmail.com>
2019-10-08 22:49:58 -0300

    Practico 4 Ej2 completo e inicio del Ej3

---
+++ Practico4/src/Main.java
@@ -0,0 +1,114 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.Statement;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class Main 
+{
+	public static void main (String[] args)
+	{
+		String error= new String();
+		String driver=null;
+		String url=null;
+		String usuario=null;
+		String password=null;
+		Connection con=null;
+		
+		try
+		{
+			/* Obtengo los datos de conexión desde el archivo de configuración */
+			Properties p=new Properties();
+			String nomArch="config/config.properties";
+			p.load(new FileInputStream(nomArch));
+			driver=p.getProperty("driver");
+			url = p.getProperty("url");
+			usuario=p.getProperty("user");
+			password=p.getProperty("password");
+
+			Class.forName(driver);
+		}
+		
+		catch (ClassNotFoundException e)
+		{
+			error= e.toString();
+		}
+		catch (FileNotFoundException fEx)
+		{
+			error= fEx.toString();
+		}
+		catch (IOException ioEx)
+		{
+			error= ioEx.toString();
+		}
+		
+		try
+		{
+			con = DriverManager.getConnection(url, usuario, password);
+			
+			DatabaseMetaData dbmtd=con.getMetaData();
+			ResultSet rstDB = dbmtd.getSchemas(null, "dbDQ");
+			con.setAutoCommit(false);
+			con.setTransactionIsolation(con.TRANSACTION_SERIALIZABLE);
+			if(!rstDB.next())
+			{		
+				String consultaCrearBD="CREATE DATABASE dbDQ;";
+				String consultaCrearTablaTemporadas = "CREATE TABLE Temporadas("
+						+ "nroTemp INT, anio INT, cantCapitulos INT,"
+						+ "PRIMARY KEY (nroTemp));";
+				String consultaCrearTablaDragQueens = "CREATE TABLE DragQueens("
+						+ "nroPart INT, nombre VARCHAR(45), cantVictorias INT DEFAULT 0, nroTemp INT, "
+						+ "PRIMARY KEY (nroPart,nombre), FOREIGN KEY (nroTemp) REFERENCES Temporadas(nroTemp)"
+						+ ");";
+				
+				System.out.print("Creando BD y tablas.\n");
+				Statement stmt=con.createStatement();
+				stmt.executeUpdate(consultaCrearBD);
+				System.out.println("DB creada.");
+				stmt.executeQuery("Use dbDQ;");
+				stmt.executeUpdate(consultaCrearTablaTemporadas);
+				System.out.println("Tabla Temporadas creada.");
+				stmt.executeUpdate(consultaCrearTablaDragQueens);
+				System.out.println("Tabla DragQueens creada.");
+				
+				String consultaCrearDatosExamenes = "INSERT INTO Examenes"
+						+ " values ('MD2012Dic','Matemática discreta','Diciembre 2012'), "
+						+ "('P12012Dic','Programación','Diciembre 2012'), "
+						+ "('BD2012Dic','Bases de datos','Diciembre 2012'), "
+						+ "('MD2013Feb','Matemática discreta','Febrero 2013');";
+				//stmt.executeUpdate(consultaCrearDatosExamenes);
+				stmt.close();
+				con.commit();
+			}
+			else
+			{
+				System.out.println("Ya existe la DB.");
+				String usarBD="Use escuelapractico3;";
+				Statement stmt=con.createStatement();
+				stmt.executeQuery(usarBD);
+				stmt.close();
+			}
+			
+			con.setAutoCommit(false);
+		}
+		catch (SQLException e)
+		{
+			try
+			{
+				con.rollback();
+			}
+			catch (SQLException ex)
+			{
+				error=ex.toString();
+			}
+			
+			error= e.toString();
+		}
+	}
+}
---
+++ Practico4/src/logicaPersistencia/fachada.java
@@ -0,0 +1,6 @@
+package logicaPersistencia;
+
+public class fachada 
+{
+	
+}
